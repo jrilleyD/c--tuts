79e0850726fc6aa3a528f1cdef4105e681d42f2d
jrilleyD <jrilley@jrilley.com>
2023-12-03 22:06:36 +0200

    Home Word 2 XOR

---
+++ XOR_HomeWork2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XOR_HomeWork2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Employee`s last name: ");
+            string lastName = Console.ReadLine();
+            Console.Write("Experience: ");
+            int experience = Convert.ToInt32(Console.ReadLine());
+
+            bool result = experience < 5 || experience >= 5 & experience < 10 || experience >= 10 & experience < 15 || experience >= 15 & experience < 25 ;
+
+            Console.ReadKey();
+        }
+    }
+}
