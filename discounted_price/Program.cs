8fcc9b2eddfe14992490ef8f4fed66a6e5988ed8
jrilleyD <jrilley@jrilley.com>
2023-12-13 22:45:15 +0200

    discounted_price add

---
+++ discounted_price/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace discounted_price
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal price = 19.75m;
+            decimal discount = 0.015m;
+            decimal discountedPrice = price - (price * discount);
+
+            Console.WriteLine($"Discounted price: {discountedPrice}");
+        }
+    }
+}
